7f6b9242c573e9418f105192528e2cefe49eee46
Harshal Purohit <82707364+HarshalPurohitEdits@users.noreply.github.com>
2023-09-13 19:40:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1757,44 +1757,44 @@ async def auto_filter(client, msg, spoll=False):
         btn.append(
             [InlineKeyboardButton(text="𝐍𝐎 𝐌𝐎𝐑𝐄 𝐏𝐀𝐆𝐄𝐒 𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄",callback_data="pages")]
         )
-    imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
+    # imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     cur_time = datetime.now(pytz.timezone('Asia/Kolkata')).time()
     time_difference = timedelta(hours=cur_time.hour, minutes=cur_time.minute, seconds=(cur_time.second+(cur_time.microsecond/1000000))) - timedelta(hours=curr_time.hour, minutes=curr_time.minute, seconds=(curr_time.second+(curr_time.microsecond/1000000)))
     remaining_seconds = "{:.2f}".format(time_difference.total_seconds())
-    TEMPLATE = script.IMDB_TEMPLATE_TXT
-    if imdb:
-        cap = TEMPLATE.format(
-            qurey=search,
-            title=imdb['title'],
-            votes=imdb['votes'],
-            aka=imdb["aka"],
-            seasons=imdb["seasons"],
-            box_office=imdb['box_office'],
-            localized_title=imdb['localized_title'],
-            kind=imdb['kind'],
-            imdb_id=imdb["imdb_id"],
-            cast=imdb["cast"],
-            runtime=imdb["runtime"],
-            countries=imdb["countries"],
-            certificates=imdb["certificates"],
-            languages=imdb["languages"],
-            director=imdb["director"],
-            writer=imdb["writer"],
-            producer=imdb["producer"],
-            composer=imdb["composer"],
-            cinematographer=imdb["cinematographer"],
-            music_team=imdb["music_team"],
-            distributors=imdb["distributors"],
-            release_date=imdb['release_date'],
-            year=imdb['year'],
-            genres=imdb['genres'],
-            poster=imdb['poster'],
-            plot=imdb['plot'],
-            rating=imdb['rating'],
-            url=imdb['url'],
-            **locals()
-        )
-        temp.IMDB_CAP[message.from_user.id] = cap
+    # TEMPLATE = settings['template']
+    # if imdb:
+    #     cap = TEMPLATE.format(
+    #         query=search,
+    #         title=imdb['title'],
+    #         votes=imdb['votes'],
+    #         aka=imdb["aka"],
+    #         seasons=imdb["seasons"],
+    #         box_office=imdb['box_office'],
+    #         localized_title=imdb['localized_title'],
+    #         kind=imdb['kind'],
+    #         imdb_id=imdb["imdb_id"],
+    #         cast=imdb["cast"],
+    #         runtime=imdb["runtime"],
+    #         countries=imdb["countries"],
+    #         certificates=imdb["certificates"],
+    #         languages=imdb["languages"],
+    #         director=imdb["director"],
+    #         writer=imdb["writer"],
+    #         producer=imdb["producer"],
+    #         composer=imdb["composer"],
+    #         cinematographer=imdb["cinematographer"],
+    #         music_team=imdb["music_team"],
+    #         distributors=imdb["distributors"],
+    #         release_date=imdb['release_date'],
+    #         year=imdb['year'],
+    #         genres=imdb['genres'],
+    #         poster=imdb['poster'],
+    #         plot=imdb['plot'],
+    #         rating=imdb['rating'],
+    #         url=imdb['url'],
+    #         **locals()
+    #     )
+    # else:
         if not settings["button"]:
             cap+="<b>\n\n<u>🍿 Your Movie Files 👇</u></b>\n"
             for file in files:
@@ -1808,65 +1808,67 @@ async def auto_filter(client, msg, spoll=False):
             for file in files:
                 cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>🀄️{get_size(file.file_size)}🎴{' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
 
-    if imdb and imdb.get('poster'):
-        try:
-            hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await m.delete()
-            try:
-                if settings['auto_delete']:
-                    await asyncio.sleep(300)
-                    await hehe.delete()
-                    await message.delete()
-            except KeyError:
-                await save_group_settings(message.chat.id, 'auto_delete', True)
-                await asyncio.sleep(300)
-                await hehe.delete()
-                await message.delete()
-        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-            pic = imdb.get('poster')
-            poster = pic.replace('.jpg', "._V1_UX360.jpg") 
-            hmm = await message.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await m.delete()
-            try:
-               if settings['auto_delete']:
-                    await asyncio.sleep(300)
-                    m=await message.reply_text("🔎")
-                    await hmm.delete()
-                    await message.delete()
-            except KeyError:
-                await save_group_settings(message.chat.id, 'auto_delete', True)
-                await asyncio.sleep(300)
-                await hmm.delete()
-                await message.delete()
-        except Exception as e:
-            logger.exception(e)
-            m=await message.reply_text("🔎") 
-            fek = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await m.delete()
-            try:
-                if settings['auto_delete']:
-                    await asyncio.sleep(300)
-                    await fek.delete()
-                    await message.delete()
-            except KeyError:
-                await save_group_settings(message.chat.id, 'auto_delete', True)
-                await asyncio.sleep(300)
-                await fek.delete()
-                await message.delete()
-    else:
-        fuk = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
-        await m.delete()
-        try:
-            if settings['auto_delete']:
-                await asyncio.sleep(300)
-                await fuk.delete()
-                await message.delete()
-        except KeyError:
-            await save_group_settings(message.chat.id, 'auto_delete', True)
+    # if imdb and imdb.get('poster'):
+    #     try:
+    #         hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+    #         await m.delete()
+    #         try:
+    #             if settings['auto_delete']:
+    #                 await asyncio.sleep(300)
+    #                 await hehe.delete()
+    #                 await message.delete()
+    #         except KeyError:
+    #             await save_group_settings(message.chat.id, 'auto_delete', True)
+    #             await asyncio.sleep(300)
+    #             await hehe.delete()
+    #             await message.delete()
+    #     except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+    #         pic = imdb.get('poster')
+    #         poster = pic.replace('.jpg', "._V1_UX360.jpg")
+    #         #m=await message.reply_text("🔎") 
+    #         hmm = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+    #         await m.delete()
+    #         try:
+    #             if settings['auto_delete']:
+    #                 await asyncio.sleep(300)
+    #                 await hmm.delete()
+    #                 await message.delete()
+    #         except KeyError:
+    #             await save_group_settings(message.chat.id, 'auto_delete', True)
+    #             await asyncio.sleep(300)
+    #             await hmm.delete()
+    #             await message.delete()
+    #     except Exception as e:
+    #         logger.exception(e)
+    #         #m=await message.reply_text("🔎") 
+    #         fek = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn))
+    #         await m.delete()
+    #         try:
+    #             if settings['auto_delete']:
+    #                 await asyncio.sleep(300)
+    #                 await fek.delete()
+    #                 await message.delete()
+    #         except KeyError:
+    #             await save_group_settings(message.chat.id, 'auto_delete', True)
+    #             await asyncio.sleep(300)
+    #             await fek.delete()
+    #             await message.delete()
+    # else:
+    fuk = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
+    
+    await m.delete()
+    try:
+        if settings['auto_delete']:
             await asyncio.sleep(300)
             await fuk.delete()
             await message.delete()
-
+    except KeyError:
+        await save_group_settings(message.chat.id, 'auto_delete', True)
+        await asyncio.sleep(300)
+        await fuk.delete()
+        await message.delete()
+    # if spoll:
+    #     await msg.message.delete()
 
 async def advantage_spell_chok(client, msg):
     mv_id = msg.id
